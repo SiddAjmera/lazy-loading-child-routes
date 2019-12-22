fe1fa06a5c47efcf0df309f0ff8124360d67495c
Siddharth Ajmera <siddajmera@outlook.com>
2019-12-22 21:52:31 +1100

    feat(chore): initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from "./not-found/not-found.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
+const routes: Routes = [
+  {
+    path: "page-not-found",
+    component: NotFoundComponent
+  },
+  {
+    path: "",
+    loadChildren: () => import("./pages/pages.module").then(m => m.PagesModule),
+    data: { preload: true }
+  },
 
-const routes: Routes = [];
+  {
+    path: "**",
+    redirectTo: "page-not-found"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
